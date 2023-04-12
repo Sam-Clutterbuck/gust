be26d0ea4c24100447305681960e6615b364a46d
Sam-Clutterbuck <129515608+Sam-Clutterbuck@users.noreply.github.com>
2023-04-12 19:35:12 +0100

    Delete core directory

--- core/src/commands_link.py
+++
@@ -1,8 +0,0 @@
-from core.src.yaml_editor import Yaml_Editor
-
-class Commands_Global:
-
-    success, yaml_file = Yaml_Editor.Yaml_Read("core/data/commands.yaml")
-    if (success == False):
-        yaml_file = {}
-    COMMANDS = yaml_file
