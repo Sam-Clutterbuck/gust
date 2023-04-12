e759c16f02deafb826778d1ab88fe2fce8dd9f6a
Sam-Clutterbuck <129515608+Sam-Clutterbuck@users.noreply.github.com>
2023-04-12 19:35:21 +0100

    Delete client directory

--- client/src/server_removal_check.py
+++
@@ -1,6 +0,0 @@
-from core.src import Integrity_Check, Gust_Log
- 
-if Integrity_Check.Dir_Check("server"):
-    Gust_Log.System_Log(500, "Attempted to run client whilst server functionality exists", None, "CLIENT")
-    print("Attempted to run client whilst server functionality exists")
-    quit()
