7211b8b5115c9e512497de3bb1a931f8abad2253
Sam-Clutterbuck <SamuelClutterbuck@outlook.com>
2023-04-12 19:53:03 +0100

    stop server and client packages being together

--- gust_client/src/server_removal_check.py
+++ gust_client/src/server_removal_check.py
@@ -1,6 +1,6 @@
 from gust_core.src import Integrity_Check, Gust_Log
  
-if Integrity_Check.Dir_Check("server"):
+if Integrity_Check.Dir_Check("gust_server"):
     Gust_Log.System_Log(500, "Attempted to run client whilst server functionality exists", None, "CLIENT")
     print("Attempted to run client whilst server functionality exists")
     quit()
